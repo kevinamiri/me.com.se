070c912de84bce505b6552601a046588a452d85e
kevin amiri <amiri990.us@gmail.com>
2020-02-01 11:43:55 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-157541223-1`,
       },
     },
     {
