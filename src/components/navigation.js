fb6833cd52e4ef8b9ad4a14c17bcf87882938db9
kevin amiri <amiri990.us@gmail.com>
2020-02-20 20:37:49 +0100

    add windows conditional and aws settings

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,12 +4,12 @@ import React, { useState } from "react";
 function Navigation() {
 
 const [scrolling, setScrolling] = useState("flex items-center justify-center flex-wrap py-6 px-8");
+if (typeof window !== 'undefined'){
+window.addEventListener('scroll', function() {
+  checkscrolling();
+});
 
-window.onscroll = function(){
-  checkscrolling()
-};
-
-function checkscrolling(){
+const checkscrolling = () => {
 
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     setScrolling("bg-graybox inset-x-0 w-full mx-auto px-6 py-6 flex items-center justify-center flex-wrap border fixed shadow-md");
@@ -17,7 +17,7 @@ function checkscrolling(){
     setScrolling("bg-graybox flex items-center justify-center flex-wrap border py-6 px-6 sticky shadow-md");
   }
 }
-
+}
   return (
   <nav className={scrolling}>
     <div className="max-w-3xl block flex-grow flex">
@@ -26,8 +26,7 @@ function checkscrolling(){
       </div>
     </div>
   </nav>
-    
   );
 }
 
-export default Navigation;
+export default Navigation
