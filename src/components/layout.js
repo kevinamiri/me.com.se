cfeadb2827fb15e670de805e1e1947a73ecde967
kevin amiri <amiri990.us@gmail.com>
2020-02-20 19:02:20 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Navigation from "../components/Navigation"
+import Navigation from "../components/navigation"
 
 class Layout extends React.Component {
   render() {
