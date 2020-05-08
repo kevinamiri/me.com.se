c277a17bd0536c87949ce0d2f01b071e89db0c0d
kevin amiri <amiri990.us@gmail.com>
2020-05-08 10:13:23 +0200

    turn to functional

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,21 +4,19 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = props => {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1 className="text-5xl font-black mt-16 mb-8">Not Found</h1>
-        <p className="mb-8">
-          You just hit a route that doesn&#39;t exist... the sadness.
-        </p>
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <h1 className="text-5xl font-black mt-16 mb-8">Not Found</h1>
+      <p className="mb-8">
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
